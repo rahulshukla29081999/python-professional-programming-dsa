903d874496b3d0e79ec04a7fa90284236101175f
Rahul Shukla <47075543+rahulshukla29081999@users.noreply.github.com>
2021-08-01 15:48:32 +0530

    Update bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -1,3 +1,14 @@
+# Bubble Sort in Python...
+#time complexity of bubble sort is O(n^2)
+#it is a simple comparision based algorithm ...
+#in this compare two adjacent element of list ..if 1st elemnt is bigger than 2nd then swap them ...
+#largest element of the list will be in last position .
+#second largest element of the list will come in second last pos.
+#this will untill,our list is sorted.
+#in this we use two loops.
+# if n=len(l)
+#for i in range (n-1)
+#for i in range (n-1)
 def bubblesort(l):
     n=len(l)
     for i in range(n-1):
